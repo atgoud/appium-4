24a0f57ae6c5225aaef3843e1842a01c31a3caff
pgq10240817 <10240817@qq.com>
2014-12-01 12:54:59 +0800

    switch 2 swiplewebdriver which is abvoe 4.2

--- appiumTest/src/test/java/com/yhpl/apptest/AndroidTest.java
+++ appiumTest/src/test/java/com/yhpl/apptest/AndroidTest.java
@@ -1,22 +1,21 @@
 package com.yhpl.apptest;
 
+import java.io.File;
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.HasTouchScreen;
 import org.openqa.selenium.interactions.TouchScreen;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteTouchScreen;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import io.selendroid.SelendroidCapabilities;
-import io.selendroid.SelendroidDriver;
-
-import java.io.File;
-import java.net.URL;
-import java.util.List;
 
 public class AndroidTest {
 	private WebDriver driver;
@@ -27,41 +26,21 @@ public class AndroidTest {
 		File classpathRoot = new File(System.getProperty("user.dir"));
 		File appDir = new File(classpathRoot, "apps/app");
 		File app = new File(appDir, "EnterpriseApp.apk");
-		// DesiredCapabilities capabilities = new DesiredCapabilities();
-		// capabilities.setCapability("device", "Android");
-		// capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-		// capabilities.setCapability(CapabilityType.VERSION, "4.4.2");
-		// capabilities.setCapability(CapabilityType.PLATFORM, "WINDOWS");
-		// capabilities.setCapability("platformName", "Android");
-		// capabilities.setCapability("deviceName", "CB5A1P3DHW");
-		//
-		// capabilities.setCapability("app", app.getAbsolutePath());
-		// capabilities.setCapability("app-package", "com.founder.eapp");
-		// capabilities.setCapability("app-activity",
-		// "com.founder.eapp.ui.activity.LoginActivity");
-		// driver = new SwipeableWebDriver(
-		// new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("device", "Android");
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+		capabilities.setCapability(CapabilityType.VERSION, "4.4.2");
+		capabilities.setCapability(CapabilityType.PLATFORM, "WINDOWS");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("deviceName", "CB5A1P3DHW");
 
-		// SelendroidCapabilities capa = new SelendroidCapabilities(
-		// "io.selendroid.testapp:0.8.0");
-		// capa.setPlatformVersion(DeviceTargetPlatform.ANDROID18);
-		// capa.setEmulator(true);
-
-		SelendroidCapabilities capa = new SelendroidCapabilities(
-				"EnterpriseApp.apk");
-
-		capa.setCapability("browserName", "");
-		capa.setCapability("deviceName", "Android Emulator");
-		// capabilities.setCapability("automationName", "Selendroid");
-		capa.setCapability("platformVersion", "4.1");
-		capa.setCapability("platformName", "Android");
-		capa.setCapability("app", app.getAbsolutePath());
-		capa.setCapability("appPackage", "com.founder.eapp");
-		capa.setCapability("appActivity",
+		capabilities.setCapability("app", app.getAbsolutePath());
+		capabilities.setCapability("app-package", "com.founder.eapp");
+		capabilities.setCapability("app-activity",
 				"com.founder.eapp.ui.activity.LoginActivity");
-		// capa.setCapability(SelendroidCapabilities., value)
-		driver = new SelendroidDriver(capa);
-		
+		driver = new SwipeableWebDriver(
+				new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
 	}
 
 	@After
